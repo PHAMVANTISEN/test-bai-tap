bdaa28ef0ce4e80f69ae4ec25c003ef3016175d6
PHAMVANTISEN <phamsen200904@gmail.com>
2024-07-27 19:20:40 +0700

    sen ned

--- Project1/Project1/Chương2.cpp
+++ Project1/Project1/Chương2.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <conio.h>
 void main()
 {
-	printf("toi la sen");
-
+	int tuoi;
+	printf("toi la sen ");
+	printf("\nso tuoi cua toi la:  ");
+	scanf_s( "%d", &tuoi);
 	return;
 }
